1716a114b15c69b69b17ad234e1e457d6b4bd478
ageurts <arno@cg.nl>
2013-06-17 09:53:48 +0200

    Config initializer fix

--- lib/generators/cg_clicktale/templates/load_config.rb
+++ lib/generators/cg_clicktale/templates/load_config.rb
@@ -1,3 +1,3 @@
 raw_config = File.read(::Rails.root + "config/clicktale.yml")
-CONFIG = YAML.load(raw_config)[::Rails.env].symbolize_keys
+CgClicktale.config = YAML.load(raw_config)[::Rails.env].symbolize_keys
 puts "Setting CLICKTALE"
